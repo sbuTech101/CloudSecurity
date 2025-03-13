c05d40d36efe73140c66c51ddda4834adee756dd
sbuTech101 <111270036+sbuTech101@users.noreply.github.com>
2025-03-13 17:19:59 +0200

    Update code main.tf

--- code main.tf
+++ code main.tf
@@ -1,5 +1,19 @@
-resource "aws_s3_bucket" "foo-bucket" {
-  # same resource configuration as previous example, but acl set for public access.
-  acl = "public-read"
+terraform {
+required_providers {
+azurerm = {
+source = "hashicorp/azurerm"
+version = "~> 3.0.2"
 }
-data "aws_caller_identity" "current" {}
+}
+ 
+required_version = ">= 1.1.0"
+}
+ 
+provider "azurerm" {
+features {}
+}
+ 
+resource "azurerm_resource_group" "rg" {
+name = "myTFResourceGroup"
+location = "westus2"
+}
